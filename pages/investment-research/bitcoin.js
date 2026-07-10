7ba9493de5d73347414b15fea39c477cb8cefa50
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-07-10 17:15:09 +0700

    change font-size

--- pages/investment-research/bitcoin.js
+++ pages/investment-research/bitcoin.js
@@ -9,7 +9,7 @@ const Bitcoin = () => {
 
             <div className="mt-16">
                 <h1 className="font-bold text-3xl md:text-4xl">Bitcoin</h1>
-                <p className="text-lg opacity-50">My thoughts about bitcoin</p>
+                <p className="text-sm md:text-lg opacity-50">My thoughts about bitcoin</p>
 
                 <p className="mt-8 text-sm md:text-lg">
                     am not gonna explain bitcoin from finance perspective, 
