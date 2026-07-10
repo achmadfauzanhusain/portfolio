f09d249977cff032ff04acb576f3886614bb0da3
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-07-10 12:26:55 +0700

    investmen research

--- pages/investment-research/bitcoin.js
+++ pages/investment-research/bitcoin.js
@@ -1,7 +1,27 @@
+import Link from "next/link"
+
 const Bitcoin = () => {
     return (
         <div className="relative w-[95%] sm:w-[90%] lg:w-[80%] mx-auto mt-16 md:mt-18">
-            <h1>Bitcoin</h1>
+            <Link href="/" className="text-blue-500 hover:text-blue-700">
+                Back
+            </Link>
+
+            <div className="mt-16">
+                <h1 className="font-bold text-4xl">Bitcoin</h1>
+                <p className="text-lg opacity-50">My thoughts about bitcoin</p>
+
+                <p className="mt-8 text-lg">
+                    am not gonna explain bitcoin from finance perspective, 
+                    because am from tech people and i think bitcoin is more about tech than finance.
+                    am more interested on bitcoin after know bitcoin is built by math.
+                    i think bitcoin is a nice example of how math can be used to solve real world problem, and how math can be used to create something that is valuable.
+                    am very interested at math because bitcoin.
+                    i think bitcoin not change paper money to bitcoin, but bitcoin change the way we think about money.
+                </p>
+            </div>
+
+            <p className="text-lg mt-8">- fauzanhusain</p>
         </div>
     )
 }
--- pages/investment-research/tempus-ai.js
+++ pages/investment-research/tempus-ai.js
@@ -1,7 +1,25 @@
+import Link from "next/link"
+
 const TempusAI = () => {
     return (
-        <div>
+        <div className="relative w-[95%] sm:w-[90%] lg:w-[80%] mx-auto mt-16 md:mt-18">
+            <Link href="/" className="text-blue-500 hover:text-blue-700">
+                Back
+            </Link>
 
+            <div className="mt-16">
+                <h1 className="font-bold text-4xl">Tempus AI</h1>
+                <p className="text-lg opacity-50">My thoughts about Tempus AI</p>
+
+                <p className="mt-8 text-lg">
+                    i think tempus ai is good company because he integrating ai into healthcare.
+                    so the company now like do many aqcuisition to integrate ai into healthcare.
+                    and tempus ai is used to help doctors to make decision, and help patients to get better treatment.
+                    tempus ai is using by many country right now, before its just united states, but now many country using it.
+                </p>
+            </div>
+            
+            <p className="text-lg mt-8">- fauzanhusain</p>
         </div>
     )
 }
